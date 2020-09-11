1ab40571ab23f66387e0ba7f2b9e03ba6318a9e7
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2020-09-11 16:25:42 +0900

    feat: use sqlite database

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,6 +1,10 @@
-use actix_web::{get, ResponseError, HttpResponse, HttpServer, App};
+use actix_web::{web, get, App, HttpResponse, HttpServer, ResponseError};
 use askama::Template;
 use thiserror::Error;
+
+use r2d2::{Pool};
+use r2d2_sqlite::{SqliteConnectionManager};
+use rusqlite::params;
 
 struct TodoEntry {
     id: u32,
@@ -10,29 +14,41 @@ struct TodoEntry {
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate {
-    entries: Vec<TodoEntry>
+    entries: Vec<TodoEntry>,
 }
 
 #[derive(Error, Debug)]
 enum MyError {
     #[error("Failed to render HTML")]
     AskamaError(#[from] askama::Error),
+    
+    #[error("Failed to get connection")]
+    ConnectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error),
 }
 
 impl ResponseError for MyError {}
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, MyError> {
+async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+    // データの準備
+    let conn = db.get()?;
+    let mut statement = conn.prepare("select id, text from todo")?;
+    let rows = statement.query_map(params![], |row| {
+        let id = row.get(0)?;
+        let text = row.get(1)?;
+        Ok(TodoEntry {
+            id, text
+        })
+    })?;
     let mut entries = Vec::new();
-    entries.push(TodoEntry {
-        id: 1,
-        text: "First entry".to_string(),
-    });
-    entries.push(TodoEntry {
-        id: 2,
-        text: "Second entry".to_string(),
-    });
+    for row in rows {
+        entries.push(row?);
+    }
 
+    // レスポンス
     let html = IndexTemplate { entries };
     let response_body = html.render()?;
 
@@ -42,8 +58,24 @@ async fn index() -> Result<HttpResponse, MyError> {
 }
 
 #[actix_rt::main]
-async fn main() -> Result<(), actix_web::Error>{
-    HttpServer::new(move || App::new().service(index))
+async fn main() -> Result<(), actix_web::Error> {
+    // データベースの準備
+    let manager = SqliteConnectionManager::file("todo.db");
+    let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
+    let conn = pool
+        .get()
+        .expect("Fai;ed to get the connection from the pool.");
+    conn.execute(
+        "create table if not exists todo (
+            id integer primary key autoincrement,
+            text text not null
+        )",
+        params![],
+    )
+    .expect("Failed to create a table `todo`.");
+
+    // サーバーを起動
+    HttpServer::new(move || App::new().service(index).data(pool.clone()))
         .bind("0.0.0.0:8080")?
         .run()
         .await?;
