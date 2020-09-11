6df973aadf9b7c21f37f3aba6adc82b5605cd7a0
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2020-09-11 11:04:58 +0900

    feat: create error handling files

--- rpn-calculator/src/bin/err_panic.rs
+++ rpn-calculator/src/bin/err_panic.rs
@@ -1,37 +1,15 @@
-// fn get_int_from_file() -> i32 {
-//     let path = "number.txt";
-//     let num_str = std::fs::read_to_string(path).expect("failed to open file");
-//     let ret = num_str
-//         .trim()
-//         .parse::<i32>()
-//         .expect("failed to parse string to a number");
-
-//     ret * 2
-// }
-
-enum MyError {
-    Io(std::io::Error),
-    Num(std::num::ParseIntError),
-}
-
-fn get_int_from_file() -> Result<i32, MyError> {
+fn get_int_from_file() -> i32 {
     let path = "number.txt";
-    let num_str = std::fs::read_to_string(path)
-        .map_err( |e| MyError::Io(e))?;
-
-    num_str
+    let num_str = std::fs::read_to_string(path).expect("failed to open file");
+    let ret = num_str
         .trim()
         .parse::<i32>()
-        .map(|t| t * 2)
-        .map_err(|e| MyError::Num(e))
+        .expect("failed to parse string to a number");
+
+    ret * 2
 }
 
+
 fn main() {
-    match get_int_from_file() {
-        Ok(x) => println!("{}", x),
-        Err(e) => match e {
-            MyError::Io(cause) => println!("I/O Error: {}", cause),
-            MyError::Num(cause) => println!("Parse Error: {}", cause),
-        }
-    }
+    println!("{}", get_int_from_file());
 }
