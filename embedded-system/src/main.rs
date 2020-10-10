a5d3322c8c726912c12d1bc09e6a393d1c9b0637
Asahi Takenouchi <le-soleil-levant.0802@outlook.com>
2020-10-10 18:48:55 +0900

    fix: use 4 LEDs

--- embedded-system/src/main.rs
+++ embedded-system/src/main.rs
@@ -13,22 +13,22 @@ fn main() -> ! {
     perip.RCC.ahb1enr.modify(|_, w| w.gpioden().set_bit());
 
     let gpiod = &perip.GPIOD;
-    // gpiod.moder.modify(|_, w| w.moder12().output());
-    // gpiod.moder.modify(|_, w| w.moder13().output());
-    // gpiod.moder.modify(|_, w| w.moder14().output());
+    gpiod.moder.modify(|_, w| w.moder12().output());
+    gpiod.moder.modify(|_, w| w.moder13().output());
+    gpiod.moder.modify(|_, w| w.moder14().output());
     gpiod.moder.modify(|_, w| w.moder15().output());
 
     loop {
-        // gpiod.bsrr.write(|w| w.bs12().set_bit());
-        // gpiod.bsrr.write(|w| w.bs13().set_bit());
-        // gpiod.bsrr.write(|w| w.bs14().set_bit());
+        gpiod.bsrr.write(|w| w.bs12().set_bit());
+        gpiod.bsrr.write(|w| w.bs13().set_bit());
+        gpiod.bsrr.write(|w| w.bs14().set_bit());
         gpiod.bsrr.write(|w| w.bs15().set_bit());
 
         delay(8_000_000);
 
-        // gpiod.bsrr.write(|w| w.br12().reset());
-        // gpiod.bsrr.write(|w| w.br13().reset());
-        // gpiod.bsrr.write(|w| w.br14().reset());
+        gpiod.bsrr.write(|w| w.br12().reset());
+        gpiod.bsrr.write(|w| w.br13().reset());
+        gpiod.bsrr.write(|w| w.br14().reset());
         gpiod.bsrr.write(|w| w.br15().reset());
 
         delay(8_000_000);
